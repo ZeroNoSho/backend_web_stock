397087653e5588bae15d2c0d3968b630204e8977
ZeroNoSho <inukuro351@gmail.com>
2023-09-03 01:43:17 +0700

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   config/Database.js

--- config/Database.js
+++ config/Database.js
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize("sql12643768", "sql12643768", "yvYE3d1GFT", {
-  host: "sql12.freesqldatabase.com",
+const db = new Sequelize("sql12643768", "sql12643768" || process.env.USER, "yvYE3d1GFT" || process.env.PASS, {
+  host: "sql12.freesqldatabase.com" || process.env.HOST,
   dialect: "mysql",
 });
 
