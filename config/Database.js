45ac59164e51f0aec55f34ca055b8cddb38bdcaf
ZeroNoSho <inukuro351@gmail.com>
2023-09-03 03:35:50 +0700

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   config/Database.js

--- config/Database.js
+++ config/Database.js
@@ -1,20 +1,17 @@
-import mysql from "mysql2";
+import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
 
 dotenv.config();
-const db = mysql.createPool({
+const db = new Sequelize(process.env.DB, process.env.USER, process.env.PASS, {
   host: process.env.HOST,
-  user: process.env.USER,
-  password: process.env.PASS,
-  database: process.env.DB,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-});
-
-db.getConnection((err, conn) => {
-  if (err) console.log(err);
-  console.log("Connected successfully");
+  dialect: "mysql",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
+  operationsAliases: false,
 });
 
 export default db;
