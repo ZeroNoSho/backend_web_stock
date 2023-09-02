5da870ea2d63f248ed6765953466101d7a4a4b67
ZeroNoSho <inukuro351@gmail.com>
2023-09-03 01:22:46 +0700

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is behind 'origin/main' by 1 commit, and can be fast-forwarded.
       (use "git pull" to update your local branch)
    
     Changes to be committed:
    	modified:   index.js

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 import express from "express";
-import dotenv from "dotenv";
-import cors from "cors";
-import cookieParser from "cookie-parser";
-import db from "./config/Database.js";
-import router from "./routes/index.js";
-dotenv.config();
+// import dotenv from "dotenv";
+// import cors from "cors";
+// import cookieParser from "cookie-parser";
+// import db from "./config/Database.js";
+// import router from "./routes/index.js";
+// dotenv.config();
 
 const app = express();
 
 //koneksi
-try {
-  await db.authenticate();
-} catch (error) {
-  console.error(error);
-}
+// try {
+//   await db.authenticate();
+// } catch (error) {
+//   console.error(error);
+// }
 
 app.use(
   cors({
@@ -30,9 +30,9 @@ app.get("/", function (req, res) {
   res.json({ nama: "Susscess" });
 });
 
-app.use(cookieParser());
-app.use(express.json());
-app.use(router);
+// app.use(cookieParser());
+// app.use(express.json());
+// app.use(router);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
