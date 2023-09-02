2c5882d6328df34547891869c79be07933962edb
ZeroNoSho <inukuro351@gmail.com>
2023-09-03 02:43:00 +0700

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   index.js

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-// import db from "./config/database.js";
-import router from "./routes/index.js";
+import db from "./config/database.js";
+// import router from "./routes/index.js";
 
 dotenv.config();
 const app = express();
