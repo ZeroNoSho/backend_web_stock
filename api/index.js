5df26b77ced79f33b1ffb585787bdb35e9f8c440
ZeroNoSho <inukuro351@gmail.com>
2023-09-03 14:23:00 +0700

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   api/index.js

--- api/index.js
+++ api/index.js
@@ -1,16 +1,33 @@
 import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import router from "./routes/index.js";
+dotenv.config();
 const app = express();
 
-app.get("/api", (req, res) => {
-  const path = `/api/item/${v4()}`;
-  res.setHeader("Content-Type", "text/html");
-  res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
-  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "PUT", "POST", "PATCH", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization", "x-csrf-token"],
+    credentials: true,
+    exposedHeaders: ["*", "Authorization"],
+    maxAge: 600,
+  })
+);
+
+app.get("/api", function (req, res) {
+  res.json({ nama: "Susscess" });
 });
 
 app.get("/api/item/:slug", (req, res) => {
   const { slug } = req.params;
   res.end(`Item: ${slug}`);
 });
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(router);;
 
 export default app;
