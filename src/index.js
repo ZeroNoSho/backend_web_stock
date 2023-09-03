bbc450b4d5bcac560a035cbcbdcbf3252dfbddcd
ZeroNoSho <inukuro351@gmail.com>
2023-09-04 01:36:53 +0700

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
 import cookieParser from "cookie-parser";
 import db from "../config/index.js";
 import { Op } from "sequelize";
-import { BahanBaku, DataBarang, Jenis, Produksi, Transaksi, Users } from "../models/index.js";
+import { BahanBaku } from "../models/index.js";
+// import router from "../routes/index.js";
 
 dotenv.config();
 const app = express();
@@ -100,70 +99,6 @@ const getBahanbakuSerch = async (req, res) => {
   });
 };
 
-//user
-const Login = async (req, res) => {
-  try {
-    const user = await Users.findOne({
-      where: {
-        name: req.body.name,
-      },
-    });
-
-    const match = await bcrypt.compare(req.body.password, user.password);
-    if (!match) return res.status(400).json({ msg: "Wrong Password" });
-    const UserId = user.id;
-    const name = user.name;
-
-    const accessToken = jwt.sign({ UserId, name }, process.env.ACCESS_TOKEN_SECRET, {
-      expiresIn: "20s",
-    });
-    const refreshToken = jwt.sign({ UserId, name }, process.env.REFRESH_TOKEN_SECRET, {
-      expiresIn: "1d",
-    });
-    await Users.update(
-      { refresh_token: refreshToken },
-      {
-        where: {
-          id: UserId,
-        },
-      }
-    );
-
-    res.cookie("refreshToken", refreshToken, {
-      httpOnly: true,
-      maxAge: 24 * 60 * 60 * 1000, // Mengubah maxAge menjadi detik
-      secure: true, // set to true if your using https or samesite is none
-      sameSite: "none",
-    });
-
-    res.json({ accessToken });
-  } catch (error) {
-    res.status(404).json({ msg: "user saalah" });
-  }
-};
-const Logout = async (req, res) => {
-  const refreshToken = req.cookies.refreshToken;
-  if (!refreshToken) return res.sendStatus(204);
-  const user = await Users.findAll({
-    where: {
-      refresh_token: refreshToken,
-    },
-  });
-  if (!user[0]) return res.sendStatus(204);
-  const useId = user[0].id;
-  await Users.update(
-    { refresh_token: null },
-    {
-      where: {
-        id: useId,
-      },
-    }
-  );
-  res.clearCookie("refreshToken");
-  return res.sendStatus(200);
-};
-
-
 app.use(
   cors({
     origin: "http://localhost:3000",
@@ -178,18 +113,14 @@ app.get("/", function (req, res) {
   res.json({ nama: "Susscess" });
 });
 
-//bahanbaku
 app.post("/Bahanbaku", setBahanbaku);
 app.patch("/Bahanbaku/:id", updateBahanbaku);
 app.delete("/Bahanbaku/:id", delBahanbaku);
 app.get("/Bahanbaku/serch", getBahanbakuSerch);
 
-//user
-app.post("/login", Login);
-app.delete("/logout", Logout);
-
 app.use(cookieParser());
 app.use(express.json());
+// app.use(router);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
