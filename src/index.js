68662af5432e7e8e1b04659dbf6fefd67ac65d74
ZeroNoSho <inukuro351@gmail.com>
2023-09-03 16:11:03 +0700

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	renamed:    config/Database.js -> config/index.js
    	modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -4,31 +4,19 @@ import dotenv from "dotenv";
 import mysql from "mysql2";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import router from "./routes/index.js";
+import db from "../config/index.js";
+// import router from "./routes/index.js";
 
 dotenv.config();
 const app = express();
 
-const db = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
-  dialect: process.env.DB_DIALECT,
-  port: process.env.DB_PORT,
-  dialectModule: mysql,
-  pool: {
-    max: 5,
-    min: 0,
-    idle: 1000,
-    handleDisconnects: true,
-  },
-});
-
-// koneksi
-try {
-  await db.authenticate();
-  console.log("DataBase Connected...");
-} catch (error) {
-  console.error(error);
-}
+//koneksi
+// try {
+//   await db.authenticate();
+//   console.log("DataBase Connected...");
+// } catch (error) {
+//   console.error(error);
+// }
 
 app.use(
   cors({
