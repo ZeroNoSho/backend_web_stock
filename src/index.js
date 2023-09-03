d2336634bb036ab80896c4ecc1658a7042080b3e
ZeroNoSho <inukuro351@gmail.com>
2023-09-03 15:39:31 +0700

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	new file:   api/index.js
    	modified:   src/index.js
    	modified:   vercel.json

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import db from "../config/database.js";
+// import db from "../config/database.js";
 // import router from "./routes/index.js";
 
 dotenv.config();
@@ -37,3 +37,5 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
+
+export default app;
