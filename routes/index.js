96a88d1443134b24341951be0b77202035e35b26
ZeroNoSho <inukuro351@gmail.com>
2023-09-04 00:56:35 +0700

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   routes/index.js
    	modified:   src/index.js

--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,82 @@
+import express from "express";
+import { verifyToken } from "../middleware/index.js";
+import {
+  Login,
+  Logout,
+  Register,
+  getUser,
+  delTransaksi,
+  getTransaksiExel,
+  getTransaksiSerch,
+  setTransaksiKeluar,
+  setTransaksiMasuk,
+  updateTransaksi,
+  refreshToken,
+  delJenis,
+  getJenisSerch,
+  setJenis,
+  updateJenis,
+  delBarang,
+  getBarangSerch,
+  setBarang,
+  updateBarang,
+  delBahanbaku,
+  getBahanbakuSerch,
+  setBahanbaku,
+  updateBahanbaku,
+  getPembelian,
+  getPembelianExel,
+  getPembeliansrch,
+  delProduksi,
+  getProduksibakuSerch,
+  setProduksi,
+  updateProduksi,
+} from "../controllers/index.js";
 
+const router = express.Router();
+
+//login
+router.get("/users", verifyToken, getUser);
+router.post("/users", Register);
+router.post("/login", Login);
+router.get("/token", refreshToken);
+router.delete("/logout", Logout);
+
+//master jenis
+router.post("/Jenis", verifyToken, setJenis);
+router.patch("/Jenis/:id", verifyToken, updateJenis);
+router.delete("/Jenis/:id", verifyToken, delJenis);
+router.get("/Jenis/serch", verifyToken, getJenisSerch);
+
+//master data barang
+router.post("/Barang", verifyToken, setBarang);
+router.patch("/Barang/:id", verifyToken, updateBarang);
+router.delete("/Barang/:id", verifyToken, delBarang);
+router.get("/Barang/serch", verifyToken, getBarangSerch);
+
+//menu bahan baku
+router.post("/Bahanbaku", verifyToken, setBahanbaku);
+router.patch("/Bahanbaku/:id", verifyToken, updateBahanbaku);
+router.delete("/Bahanbaku/:id", verifyToken, delBahanbaku);
+router.get("/Bahanbaku/serch", verifyToken, getBahanbakuSerch);
+
+//produksi
+router.get("/Produksi/serch", verifyToken, getProduksibakuSerch);
+router.post("/Produksi", verifyToken, setProduksi);
+router.delete("/Produksi/:id", verifyToken, delProduksi);
+router.patch("/Produksi/:id", verifyToken, updateProduksi);
+
+//transaksi Masuk
+router.post("/Transaksi/Masuk", verifyToken, setTransaksiMasuk);
+router.post("/Transaksi/Keluar", verifyToken, setTransaksiKeluar);
+router.patch("/Transaksi/:id", updateTransaksi);
+router.delete("/Transaksi/:id", verifyToken, delTransaksi);
+router.get("/Transaksi/serch", verifyToken, getTransaksiSerch);
+router.get("/Transaksi/exel", getTransaksiExel);
+
+//Rencana Pembelian
+router.get("/Pembelian", verifyToken, getPembelian);
+router.get("/Pembelian/serch", verifyToken, getPembeliansrch);
+router.get("/Pembelian/exel", getPembelianExel);
+
+export default router;
--- src/index.js
+++ src/index.js
@@ -3,39 +3,8 @@ import dotenv from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import db from "../config/index.js";
-import { verifyToken } from "../middleware/index.js";
-import {
-  Login,
-  Logout,
-  Register,
-  getUser,
-  delTransaksi,
-  getTransaksiExel,
-  getTransaksiSerch,
-  setTransaksiKeluar,
-  setTransaksiMasuk,
-  updateTransaksi,
-  refreshToken,
-  delJenis,
-  getJenisSerch,
-  setJenis,
-  updateJenis,
-  delBarang,
-  getBarangSerch,
-  setBarang,
-  updateBarang,
-  delBahanbaku,
-  getBahanbakuSerch,
-  setBahanbaku,
-  updateBahanbaku,
-  getPembelian,
-  getPembelianExel,
-  getPembeliansrch,
-  delProduksi,
-  getProduksibakuSerch,
-  setProduksi,
-  updateProduksi,
-} from "../controllers/index.js";
+import { Sequelize } from "sequelize";
+// import router from "../routes/index.js";
 
 dotenv.config();
 const app = express();
@@ -47,6 +16,35 @@ try {
 } catch (error) {
   console.error(error);
 }
+
+const { DataTypes } = Sequelize;
+const BahanBaku = db.define(
+  "bahanbaku",
+  {
+    nama: {
+      type: DataTypes.STRING,
+    },
+    jenis: {
+      type: DataTypes.STRING,
+    },
+    stok: {
+      type: DataTypes.INTEGER,
+    },
+    tipe: {
+      type: DataTypes.INTEGER,
+    },
+  },
+  { freezeTableName: true }
+);
+
+const getBahanbaku = async (req, res) => {
+  try {
+    const Databarang = await BahanBaku.findAll();
+    res.json(Databarang);
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 app.use(
   cors({
@@ -62,53 +60,11 @@ app.use(
 app.get("/", function (req, res) {
   res.json({ nama: "Susscess" });
 });
-
-//login
-app.get("/users", verifyToken, getUser);
-app.post("/users", Register);
-app.post("/login", Login);
-app.get("/token", refreshToken);
-app.delete("/logout", Logout);
-
-//master jenis
-app.post("/Jenis", verifyToken, setJenis);
-app.patch("/Jenis/:id", verifyToken, updateJenis);
-app.delete("/Jenis/:id", verifyToken, delJenis);
-app.get("/Jenis/serch", verifyToken, getJenisSerch);
-
-//master data barang
-app.post("/Barang", verifyToken, setBarang);
-app.patch("/Barang/:id", verifyToken, updateBarang);
-app.delete("/Barang/:id", verifyToken, delBarang);
-app.get("/Barang/serch", verifyToken, getBarangSerch);
-
-//menu bahan baku
-app.post("/Bahanbaku", verifyToken, setBahanbaku);
-app.patch("/Bahanbaku/:id", verifyToken, updateBahanbaku);
-app.delete("/Bahanbaku/:id", verifyToken, delBahanbaku);
-app.get("/Bahanbaku/serch", verifyToken, getBahanbakuSerch);
-
-//produksi
-app.get("/Produksi/serch", verifyToken, getProduksibakuSerch);
-app.post("/Produksi", verifyToken, setProduksi);
-app.delete("/Produksi/:id", verifyToken, delProduksi);
-app.patch("/Produksi/:id", verifyToken, updateProduksi);
-
-//transaksi Masuk
-app.post("/Transaksi/Masuk", verifyToken, setTransaksiMasuk);
-app.post("/Transaksi/Keluar", verifyToken, setTransaksiKeluar);
-app.patch("/Transaksi/:id", updateTransaksi);
-app.delete("/Transaksi/:id", verifyToken, delTransaksi);
-app.get("/Transaksi/serch", verifyToken, getTransaksiSerch);
-app.get("/Transaksi/exel", getTransaksiExel);
-
-//Rencana Pembelian
-app.get("/Pembelian", verifyToken, getPembelian);
-app.get("/Pembelian/serch", verifyToken, getPembeliansrch);
-app.get("/Pembelian/exel", getPembelianExel);
+app.get("/Bahanbaku", getBahanbaku);
 
 app.use(cookieParser());
 app.use(express.json());
+// app.use(router);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
