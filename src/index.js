8a59693dfdd362998bb1e11264d5b1b5b67444bc
ZeroNoSho <inukuro351@gmail.com>
2023-09-04 02:40:24 +0700

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -110,6 +110,29 @@ const Logout = async (req, res) => {
   res.clearCookie("refreshToken");
   return res.sendStatus(200);
 };
+const refreshToken = async (req, res) => {
+  try {
+    const refreshToken = req.cookies.refreshToken;
+    if (!refreshToken) return res.sendStatus(401);
+    const user = await Users.findAll({
+      where: {
+        refresh_token: refreshToken,
+      },
+    });
+    if (!user[0]) return res.sendStatus(403);
+    jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, decode) => {
+      if (err) return res.sendStatus(403);
+      const userId = user[0].id;
+      const name = user[0].name;
+      const accessToken = jwt.sign({ userId, name }, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "15s",
+      });
+      res.json({ accessToken });
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 //bahan
 const SetBahanbaku = async (req, res) => {
@@ -825,6 +848,12 @@ app.use(
 app.get("/", function (req, res) {
   res.json({ nama: "Susscess" });
 });
+
+app.get("/users", verifyToken, getUser);
+app.post("/users", Register);
+app.post("/login", Login);
+app.get("/token", refreshToken);
+app.delete("/logout", Logout);
 
 app.post("/Barang", verifyToken, setBarang);
 app.patch("/Barang/:id", verifyToken, updateBarang);
