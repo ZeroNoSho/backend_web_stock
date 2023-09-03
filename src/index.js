db3454ef2f1b989a64b6450b4243a3d01c37a921
ZeroNoSho <inukuro351@gmail.com>
2023-09-04 01:41:28 +0700

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   controller/index.js
    	modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -3,8 +3,7 @@ import dotenv from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import db from "../config/index.js";
-import { Op } from "sequelize";
-import { BahanBaku, DataBarang, Jenis, Produksi, Transaksi, Users } from "../models/index.js";
+import { delBahanbaku, getBahanbakuSerch, setBahanbaku, updateBahanbaku } from "../controller/index.js";
 // import router from "../routes/index.js";
 
 dotenv.config();
@@ -17,87 +16,6 @@ try {
 } catch (error) {
   console.error(error);
 }
-
-//bahan baku
-const setBahanbaku = async (req, res) => {
-  const { nama, jenis, stok } = req.body;
-  const product = await BahanBaku.findOne({
-    where: {
-      nama: nama,
-    },
-  });
-  if (product) return res.json({ msg: "sudah ada" });
-
-  try {
-    await BahanBaku.create({
-      nama: nama,
-      jenis: jenis,
-      stok: stok,
-      tipe: "Bahan",
-    });
-    res.json({ msg: "berhasil menambahkan" });
-  } catch (error) {
-    console.log(error);
-  }
-};
-const updateBahanbaku = async (req, res) => {
-  const product = await BahanBaku.findOne({
-    where: {
-      id: req.params.id,
-    },
-  });
-  if (!product) return res.status(404).json({ msg: "No data" });
-
-  const { nama, jenis, stok } = req.body;
-  try {
-    await BahanBaku.update(
-      { nama: nama, jenis: jenis, stok: stok },
-      {
-        where: {
-          id: req.params.id,
-        },
-      }
-    );
-    res.status(200).json({ msg: "berhasil di update" });
-  } catch (error) {
-    console.log(error.message);
-  }
-};
-const delBahanbaku = async (req, res) => {
-  const product = await BahanBaku.findOne({
-    where: {
-      id: req.params.id,
-    },
-  });
-  if (!product) return res.status(404).json({ msg: "No data" });
-  try {
-    await BahanBaku.destroy({
-      where: {
-        id: req.params.id,
-      },
-    });
-    res.status(200).json({ msg: "berhasil di hapus" });
-  } catch (error) {
-    console.log(error.message);
-  }
-};
-const getBahanbakuSerch = async (req, res) => {
-  const search = req.query.search_query || "";
-  const result = await BahanBaku.findAll({
-    where: {
-      [Op.or]: [
-        {
-          nama: {
-            [Op.like]: "%" + search + "%",
-          },
-        },
-      ],
-    },
-  });
-  res.json({
-    result: result,
-  });
-};
 
 app.use(
   cors({
@@ -112,11 +30,6 @@ app.use(
 app.get("/", function (req, res) {
   res.json({ nama: "Susscess" });
 });
-
-app.post("/Bahanbaku", setBahanbaku);
-app.patch("/Bahanbaku/:id", updateBahanbaku);
-app.delete("/Bahanbaku/:id", delBahanbaku);
-app.get("/Bahanbaku/serch", getBahanbakuSerch);
 
 app.use(cookieParser());
 app.use(express.json());
