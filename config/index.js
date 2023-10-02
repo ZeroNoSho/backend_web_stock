b3b925709b0c3561c37b32da1cc331f139d6513d
ZeroNoSho <inukuro351@gmail.com>
2023-10-02 23:02:58 +0700

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   config/index.js

--- config/index.js
+++ config/index.js
@@ -1,8 +1,5 @@
 import { Sequelize } from "sequelize";
-import dotenv from "dotenv";
-import mysql from "mysql2";
 
-dotenv.config();
 const db = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   dialect: process.env.DB_DIALECT,
