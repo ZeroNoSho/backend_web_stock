b7f6bb72a81da4297aeb265d4812cf4f71cfade5
ZeroNoSho <inukuro351@gmail.com>
2023-10-02 22:51:33 +0700

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -867,72 +867,72 @@ app.use(express.json());
 // };
 
 //pembelian
-const getPembelian = async (req, res) => {
-  const number = parseInt(req.query.limit) || 100;
-  try {
-    const Barang = await DataBarang.findAll();
-    const Bahan = await BahanBaku.findAll();
-    const arrayall = [...Bahan, ...Barang];
-    const all = [];
+// const getPembelian = async (req, res) => {
+//   const number = parseInt(req.query.limit) || 100;
+//   try {
+//     const Barang = await DataBarang.findAll();
+//     const Bahan = await BahanBaku.findAll();
+//     const arrayall = [...Bahan, ...Barang];
+//     const all = [];
 
-    arrayall.forEach((item) => {
-      if (item.stok <= number) {
-        all.push(item);
-      }
-    });
+//     arrayall.forEach((item) => {
+//       if (item.stok <= number) {
+//         all.push(item);
+//       }
+//     });
 
-    res.json(all);
-  } catch (error) {
-    console.error(error);
-  }
-};
-const getPembeliansrch = async (req, res) => {
-  const search = req.query.search_query || "";
-  const Barang = await DataBarang.findAll();
+//     res.json(all);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// };
+// const getPembeliansrch = async (req, res) => {
+//   const search = req.query.search_query || "";
+//   const Barang = await DataBarang.findAll();
 
-  const Bahan = await BahanBaku.findAll();
+//   const Bahan = await BahanBaku.findAll();
 
-  const arrayall = [...Bahan, ...Barang];
+//   const arrayall = [...Bahan, ...Barang];
 
-  const searchResults = arrayall.filter((item) => item.nama.includes(search));
+//   const searchResults = arrayall.filter((item) => item.nama.includes(search));
 
-  res.json(searchResults);
-};
-const getPembelianExel = async (req, res) => {
-  const Barang = await DataBarang.findAll();
-  const Bahan = await BahanBaku.findAll();
+//   res.json(searchResults);
+// };
+// const getPembelianExel = async (req, res) => {
+//   const Barang = await DataBarang.findAll();
+//   const Bahan = await BahanBaku.findAll();
 
-  const arrayall = [...Bahan, ...Barang];
-  const heading = [
-    [
-      "Nama Barang / Bahan",
-      " satuan",
-      "stok",
-      "tipe",
-      "createdAt",
-      "updatedAt",
-    ],
-  ];
+//   const arrayall = [...Bahan, ...Barang];
+//   const heading = [
+//     [
+//       "Nama Barang / Bahan",
+//       " satuan",
+//       "stok",
+//       "tipe",
+//       "createdAt",
+//       "updatedAt",
+//     ],
+//   ];
 
-  const multiDimensionalArray = arrayall.map((obj) => [
-    obj.nama,
-    obj.jenis,
-    obj.stok,
-    obj.tipe,
-    obj.createdAt.toString(),
-    obj.updatedAt.toString(),
-  ]);
+//   const multiDimensionalArray = arrayall.map((obj) => [
+//     obj.nama,
+//     obj.jenis,
+//     obj.stok,
+//     obj.tipe,
+//     obj.createdAt.toString(),
+//     obj.updatedAt.toString(),
+//   ]);
 
-  const workbook = XLSX.utils.book_new();
-  const worksheet = XLSX.utils.aoa_to_sheet(multiDimensionalArray);
+//   const workbook = XLSX.utils.book_new();
+//   const worksheet = XLSX.utils.aoa_to_sheet(multiDimensionalArray);
 
-  XLSX.utils.sheet_add_aoa(worksheet, heading);
-  XLSX.utils.book_append_sheet(workbook, worksheet, "books");
+//   XLSX.utils.sheet_add_aoa(worksheet, heading);
+//   XLSX.utils.book_append_sheet(workbook, worksheet, "books");
 
-  const buffer = XLSX.write(workbook, { bookType: "xlsx", type: "buffer" });
-  res.attachment("PersedianData.xlsx");
-  return res.send(buffer);
-};
+//   const buffer = XLSX.write(workbook, { bookType: "xlsx", type: "buffer" });
+//   res.attachment("PersedianData.xlsx");
+//   return res.send(buffer);
+// };
 
 app.get("/", function (req, res) {
   res.json({ nama: "Susscess" });
@@ -971,9 +971,9 @@ app.get("/", function (req, res) {
 // app.get("/Transaksi/serch", verifyToken, getTransaksiSerch);
 // app.get("/Transaksi/exel", getTransaksiExel);
 
-app.get("/Pembelian", verifyToken, getPembelian);
-app.get("/Pembelian/serch", verifyToken, getPembeliansrch);
-app.get("/Pembelian/exel", getPembelianExel);
+// app.get("/Pembelian", verifyToken, getPembelian);
+// app.get("/Pembelian/serch", verifyToken, getPembeliansrch);
+// app.get("/Pembelian/exel", getPembelianExel);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
